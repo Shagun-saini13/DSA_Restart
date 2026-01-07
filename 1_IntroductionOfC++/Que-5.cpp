306e53ab8fa6d7a70011cdfe4db600c9281290d4
Shagun-saini13 <sainishagun001@gmail.com>
2026-01-07 18:32:58 +0530

    introduction

--- 1_IntroductionOfC++/Que-5.cpp
+++ 1_IntroductionOfC++/Que-5.cpp
@@ -7,3 +7,8 @@
 //1st complement -> 01111
 //2nd complement ->    +1
 // -16           -> 10000
+
+//So how computer get know it is -16 for 10000 and not 16? as for both binary number is same.
+//Answer->
+//The same hardware can treat the same bits as signed or unsigned
+//The difference is how the instruction interprets them
---
+++ 3/3.cpp
@@ -0,0 +1,7 @@
+//que 1-> Two numbers are given, print their product
+#include<iostream>
+using namespace std;
+int main(){
+    int a=4, b=3;
+    cout<<a*b;
+}
